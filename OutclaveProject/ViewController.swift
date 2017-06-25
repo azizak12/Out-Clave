ff10d4911ab31835526a2c51a28eadfa7f5eb38f
azizak12 <azizak712522@gmail.com>
2017-06-25 10:16:49 -0400

    Add files via upload

---
+++ OutclaveProject/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Outclave
+//
+//  Created by Natalie Park on 6/24/17.
+//  Copyright © 2017 Natalie, Bernadette, Aziza, Chin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    
+    @IBOutlet weak var tfInput: UITextField!
+    @IBOutlet weak var viewColor: UIView!
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var picker: UIPickerView!
+    
+    let languages = ["English", "Español", "普通话", "हिंदी", "Français", "Italiano", "Deutsche", "日本語",  "فارسی", "Pусский"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) ->  Int
+    {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return languages[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return languages.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        label.text = languages[row]
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+}
